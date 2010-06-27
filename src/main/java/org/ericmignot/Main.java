bca816e65d52e46149b6005b3361b788cdc8fd4a
testaddict <eric@macbook-pro-de-eric-mignot-3.local>
2010-06-27 10:44:53 +0200

    port en paramètre

--- src/main/java/org/ericmignot/Main.java
+++ src/main/java/org/ericmignot/Main.java
@@ -8,7 +8,12 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(80);
+    	int port = 8080;
+    	if (args.length > 0) {
+    		port = Integer.parseInt(args[0]);
+    	}
+    	
+        Server server = new Server(port);
         
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[] { new FileHandler(), new PageHandler() });
