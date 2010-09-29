b05b1bc2e0f1f6d1db0bf493ec6c68675e3a3867
Bob <bob@pc-ubuntu.(none)>
2010-09-28 21:00:24 -0400

    unused code

--- src/main/java/org/ericmignot/adapters/LegacyPage.java
+++
@@ -1,9 +0,0 @@
-package org.ericmignot.adapters;
-
-import java.io.IOException;
-
-public interface LegacyPage {
-
-	public String content() throws IOException;
-
-}
--- src/test/java/org/ericmignot/util/DocumentBuilder.java
+++ src/test/java/org/ericmignot/util/DocumentBuilder.java
@@ -9,18 +9,12 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.ericmignot.adapters.LegacyPage;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class DocumentBuilder {
 
-	public static Element doc( LegacyPage page ) throws IOException {
-		String content = page.content();
-		return doc( content );
-	}
-	
 	public static Element doc( String content ) throws IOException {
 		InputStream is = null;
 		try {
